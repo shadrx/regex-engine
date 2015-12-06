bc0852307d699c720efaae6c81787bc9aa85dd04
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-12-07 10:49:17 +1300

    Split 'constraint' and 'constraints' into separate files

---
+++ lib/constraints.rb
@@ -0,0 +1,72 @@
+require_relative 'constraint'
+
+module Constraints
+  # Checks that a set of characters are equal to a value.
+  # Regex: /foo/
+  class Eq < Constraint
+    def initialize(text)
+      @text = text
+    end
+
+    def matches(text)
+      if @text == text
+        [Match.new(text)]
+      else
+        []
+      end
+    end
+  end
+
+  # Regex: /(abc|def)/
+  class Or < Constraint
+    def initialize(constraints)
+      @constraints = constraints
+    end
+
+    def matches(value)
+      @constraints.flat_map { |constraint| constraint.matches(value) }
+    end
+  end
+
+  # Regex: /./
+  class AnyCharacter < Constraint
+    def initialize; end
+  end
+
+  # Regex: /abc+/
+  # Matches at least one `abc` token.
+  class RepeatOneOrMore < Constraint
+    def initialize(constraint)
+      @constraint = constraint
+    end
+
+    def matches(value)
+    end
+  end
+
+  # Regex: /abc*/
+  # Matches 0 or more `abc` tokens.
+  class RepeatAnyAmount < Constraint
+    def initialize(constraint)
+      @constraint = constraint
+    end
+
+    def matches(value)
+    end
+  end
+
+  # Regex: /abc{2,3}/
+  # Matches `abc` at least twice and at most three times.
+  class RepeatExactTimes < Constraint
+    def initialize(constraint, min, max)
+      @constraint = constraint
+      @min = min
+      @max = max
+    end
+
+    def matches(value)
+    end
+  end
+end
+
+
---
+++ spec/constraints_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../lib/constraints'
+
+describe Constraints::Eq do
+
+end
+
+describe Constraints::Or do
+
+end
+
+describe Constraints::AnyCharacter do
+
+end
+
+describe Constraints::RepeatOneOrMore do
+
+end
+
+describe Constraints::RepeatAnyAmount do
+
+end
+
+describe Constraints::RepeatExactTimes do
+
+end
